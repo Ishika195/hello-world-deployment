aa98faed3dcf7063fff3bb26c06be009897427b0
Ishika195 <91590352+Ishika195@users.noreply.github.com>
2021-11-24 10:57:41 +0530

    first commit

---
+++ src/main/java/com/example/helloishika/HelloishikaApplication.java
@@ -0,0 +1,13 @@
+package com.example.helloishika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloishikaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloishikaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/helloishika/HelloishikaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.helloishika;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloishikaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
